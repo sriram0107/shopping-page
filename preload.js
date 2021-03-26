9bdecc83969d22ce1848599899058a2ad21e25f3
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2021-03-26 21:08:17 +0400

    finished modal template

--- preload.js
+++ preload.js
@@ -18,14 +18,40 @@ const mock = {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
+  const toggleModal = (product) => {
+    const modal = document.querySelector(".modal");
+    const close = document.querySelector(".close");
   };
 
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
+  const createCard = (product, catalogue) => {
+    const card = document.createElement("div");
+    const image_name = product.productImage.slice(30); // gets the name of the image (lorempixel API too slow)
+    const bg = document.createElement("div");
+    bg.setAttribute(
+      "style",
+      `background-image:url(../assets/lorem_img/${image_name}.jpg); background-size:cover;background-repeat:no-repeat;`
+    );
+    card.className = "card";
+
+    const card_details = document.createElement("div");
+    card_details.className = "card_details";
+
+    const heading = document.createElement("h4");
+    heading.textContent = product.productName;
+
+    const cost = document.createElement("h5");
+    cost.textContent = "₹" + product.productCost;
+
+    const desc = document.createElement("p");
+    desc.textContent = product.productSummary;
+
+    card.appendChild(bg);
+    card_details.appendChild(heading);
+    card_details.appendChild(cost);
+    card_details.appendChild(desc);
+    card.appendChild(card_details);
+    catalogue.appendChild(card);
+  };
 
   const getData = async () => {
     const catalogue = document.querySelector(".catalogue");
@@ -33,33 +59,9 @@ window.addEventListener("DOMContentLoaded", () => {
     try {
       const res = await fetch(API_ENDPOINT);
       const data = await res.json();
-      loader.style.display = "none";
-      var pics = [];
-      console.log(pics);
+      loader.style.display = "none"; // turn off loading icon
       data.forEach((product) => {
-        const card = document.createElement("div");
-        const image_name = product.productImage.slice(30); // gets the name of the image
-        console.log(image_name);
-        const bg = document.createElement("div");
-        bg.setAttribute(
-          "style",
-          `background-image:url(../assets/lorem_img/${image_name}.jpg); background-size:cover;background-repeat:no-repeat;`
-        );
-        card.className = "card";
-        const card_details = document.createElement("div");
-        card_details.className = "card_details";
-        const heading = document.createElement("h4");
-        heading.textContent = product.productName;
-        const cost = document.createElement("h5");
-        cost.textContent = "₹" + product.productCost;
-        const desc = document.createElement("p");
-        desc.textContent = product.productSummary;
-        card.appendChild(bg);
-        card_details.appendChild(heading);
-        card_details.appendChild(cost);
-        card_details.appendChild(desc);
-        card.appendChild(card_details);
-        catalogue.appendChild(card);
+        createCard(product, catalogue);
       });
     } catch (err) {
       console.log(err);
